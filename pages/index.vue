d9031f43a12210ef0e73790274a863100e409ffc
Luke Bouch <lukebouch2014@gmail.com>
2022-04-04 21:46:18 -0400

    Add more microformats

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,9 @@
   <div>
     <section class="h-screen flex justify-center items-center text-center">
       <div class="container p-5">
-        <img class="w-32 mx-auto mb-12 shadow-xl rounded-full" src="@/assets/images/profile.png" alt="A photo of Luke Bouch.">
+        <img class="w-32 mx-auto mb-12 shadow-xl rounded-full u-photo" src="@/assets/images/profile.png" alt="A photo of Luke Bouch.">
         <h1 class="leading-10">Hello 👋<br>
-        <span class="text-lg font-normal">my name is <a href="https://lukebouch.com" class="h-card">Luke Bouch</a>.</span></h1>
+        <span class="text-lg font-normal">my name is <a href="https://lukebouch.com" class="h-card p-name">Luke Bouch</a>.</span></h1>
       </div>
     </section>
     <section class="container-sm p-5">
