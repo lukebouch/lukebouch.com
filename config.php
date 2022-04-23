06cc3758e16ba4f5f017c29140a943ce6228f705
Luke Bouch <lukebouch2014@gmail.com>
2022-04-23 09:46:02 -0400

    Update site meta

--- config.php
+++ config.php
@@ -3,7 +3,31 @@
 return [
     'production' => false,
     'baseUrl' => '',
-    'title' => 'Jigsaw',
-    'description' => 'Website description.',
-    'collections' => [],
+    'title' => 'Luke Bouch',
+    'description' => 'The personal blog of Luke Bouch.',
+    'collections' => [
+        'posts' => [
+            'path' => 'blog/{date|Y-m-d}/{slug}',
+            'sort' => 'date',
+            'items' => function ($config) {
+                $context = stream_context_create([
+                    'http' => [
+                        'header' => 'Authorization: Bearer ' . env('API_TOKEN'),
+                    ]
+                ]);
+
+                $response = json_decode(file_get_contents('https://' . getenv('API_URL') . '/posts', false, $context));
+                $posts = $response->data;
+
+                return collect($posts)->map(function ($post) {
+                    return [
+                        'title' => $post->title,
+                        'slug' => $post->slug,
+                        'date' => $post->published_at,
+                        'html' => $post->html_content,
+                    ];
+                });
+            }
+        ]
+    ],
 ];
