0838e4daec431ac5dcbbdfd291cb742d048b56ec
Luke Bouch <lukebouch2014@gmail.com>
2022-04-25 17:56:12 -0400

    Update rss.blade.php

--- source/_layouts/rss.blade.php
+++ source/_layouts/rss.blade.php
@@ -2,7 +2,7 @@
 <feed xmlns="http://www.w3.org/2005/Atom">
     <title>{{ $page->siteName }}</title>
     <link href="{{ rightTrimPath($page->baseUrl) }}/{{ $page->site_path }}" />
-    <link type="application/atom+xml" rel="self" href="{{ $page->getUrl() }}" />
+    <link type="application/atom+xml" rel="self" href="{{ rightTrimPath($page->baseUrl) }}/{{ $page->getUrl() }}" />
     <updated>{{ date(DATE_ATOM) }}</updated>
     <id>{{ $page->getUrl() }}</id>
     <author>
