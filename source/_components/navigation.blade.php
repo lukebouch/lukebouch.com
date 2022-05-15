bcc06c3e93b0a21197b703496b4e46dcf359fbd2
Luke Bouch <luke@lukebouch.com>
2022-05-15 19:40:55 -0400

    Improved mobile navigation

--- source/_components/navigation.blade.php
+++ source/_components/navigation.blade.php
@@ -1,8 +1,8 @@
 <header class="bg-yellow-500">
     <nav class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8" aria-label="Top">
-        <div class="flex items-center justify-center w-full py-6 border-b border-indigo-500 lg:border-none">
+        <div class="hidden items-center justify-center w-full py-6 lg:flex">
             <div class="flex items-center">
-                <div class="hidden ml-10 space-x-8 lg:block">
+                <div class="ml-10 space-x-8">
                     <x-desktop-link href="/" :page="$page">Home</x-desktop-link>
                     <x-desktop-link href="/blog" :page="$page">Blog</x-desktop-link>
                 </div>
