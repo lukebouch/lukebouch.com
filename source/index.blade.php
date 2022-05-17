fd725e0d5dfbb3177e6cad728e9925a9e78cad49
Luke Bouch <luke@lukebouch.com>
2022-05-16 22:36:00 -0400

    Remove recent posts section

--- source/index.blade.php
+++ source/index.blade.php
@@ -16,43 +16,4 @@ pagination: { collection: posts, perPage: 5}
             </h1>
         </div>
     </section>
-    <section class="container-sm">
-        <div class="bg-white pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-            <div class="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
-                <div>
-                    <h2>Recent Posts</h2>
-                </div>
-                <div class="mt-6 pt-10 grid gap-16 lg:grid-cols-2 lg:gap-x-5 lg:gap-y-12">
-                    @foreach ($pagination->items as $post)
-                        <article class="h-entry">
-                            <p class="text-sm text-gray-500">
-                                <time datetime="2020-03-16">{{ date('g:ia \o\n F j, Y', strtotime($post->date)) }}</time>
-                            </p>
-                            <a href="{{ $post->getUrl() }}" class="u-url mt-2 block">
-                                @if ($post->name)
-                                    <p class="text-xl font-semibold text-gray-900 ">{{ $post->name }}</p>
-                                @endif
-                                <div class="mt-3 text-base text-gray-500 e-content post-content">{!! $page->getExcerpt($post->html, 255) !!}</div>
-                            </a>
-                            <div class="mt-3">
-                                <a href="#" class="text-base font-semibold text-yellow-600 hover:text-yellow-500"> Read
-                                    Post</a>
-                            </div>
-                            <div class="hidden dt-published">{{ $post->date }}</div>
-                            <a rel="author" class="hidden p-author h-card" href="https://lukebouch.com">Luke Bouch</a>
-                        </article>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="px-5 container-sm">
-        <ol class="space-y-8 divide-y">
-            <li>
-                <article class="pt-8 h-entry">
-
-                </article>
-            </li>
-        </ol>
-    </section>
 @endsection
