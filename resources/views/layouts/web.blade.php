38bcaa2afa937b1fd3283760dc771559e09fc196
Luke Bouch <luke@lukebouch.com>
2022-05-30 18:12:38 -0400

    WIP

---
+++ resources/views/layouts/web.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link href="TODO" rel="alternate" type="application/rss+xml" title="{{ config('web.title') }}">
+
+    <title>{{ config('web.title') }}</title>
+
+    <!-- Fonts -->
+
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+
+<body>
+    <main>
+        {{ $slot }}
+    </main>
+</body>
+
+</html>
