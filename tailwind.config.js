d909fb803b5113d5f3733960ec90b2ad307b56eb
Luke Bouch <luke@lukebouch.com>
2022-05-31 15:39:18 -0400

    Install Tailwindcss typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,9 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/line-clamp'),
     ],
 };
