36210b3a0ca689f3e7f3718d20def35e85408a06
Luke Bouch <luke@lukebouch.com>
2022-05-31 15:48:52 -0400

    Center navigation links

--- resources/views/components/web/navigation/index.blade.php
+++ resources/views/components/web/navigation/index.blade.php
@@ -2,7 +2,7 @@
     <nav class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8" aria-label="Top">
         <div class="items-center justify-center hidden w-full py-6 lg:flex">
             <div class="flex items-center">
-                <div class="ml-10 space-x-8">
+                <div class="space-x-8">
                     <x-web.navigation.desktop-link href="/">Home</x-web.navigation.desktop-link>
                     <x-web.navigation.desktop-link href="/blog">Blog</x-web.navigation.desktop-link>
                 </div>
