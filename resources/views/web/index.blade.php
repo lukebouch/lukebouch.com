fb8704549c6f22388795fe3399efe1550accfa0a
Luke Bouch <luke@lukebouch.com>
2022-06-11 22:14:31 -0400

    Fix desktop

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1,16 +1,16 @@
 <x-web-layout>
-    <section class="container flex items-center px-4">
+    <section class="container flex items-center px-6">
         <div class="flex-1">
             <h1>Luke Bouch</h1>
             <p>I'm a junior developer at Wilber Group where I aid in the development and maintance of custom business
                 applications.</p>
         </div>
-        <div class="w-1/3 flex-inital">
+        <div class="w-1/2 md:w-1/3 flex-inital">
             <img class=" drop-shadow-md" src="/images/lukebouch-cutout.png" alt="Luke Bouch">
         </div>
     </section>
-    <section class="max-w-6xl px-4 py-10 mx-auto">
-        <div class="px-6 py-4 text-white rounded-md shadow-xl bg-sky-500">
+    <section class="max-w-6xl px-4 mx-auto">
+        <div class="px-6 py-8 text-white rounded-md shadow-xl bg-sky-500">
             <div class="container space-y-5">
                 <h2>Recent Blog Posts</h2>
                 @unless($posts->isEmpty())
