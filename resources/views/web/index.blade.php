b0139cc8167a70ed72ab240c57ede95620906a67
Luke Bouch <luke@lukebouch.com>
2022-06-11 22:09:23 -0400

    Fix mobile

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1,5 +1,5 @@
 <x-web-layout>
-    <section class="container flex items-center">
+    <section class="container flex items-center px-4">
         <div class="flex-1">
             <h1>Luke Bouch</h1>
             <p>I'm a junior developer at Wilber Group where I aid in the development and maintance of custom business
@@ -9,24 +9,26 @@
             <img class=" drop-shadow-md" src="/images/lukebouch-cutout.png" alt="Luke Bouch">
         </div>
     </section>
-    <section class="max-w-6xl py-10 mx-auto text-white rounded-md shadow-xl bg-sky-500">
-        <div class="container space-y-5">
-            <h2>Recent Blog Posts</h2>
-            @unless($posts->isEmpty())
-                <ol class="grid grid-cols-3 gap-3">
-                    @foreach ($posts as $post)
-                        <li class="space-y-2">
-                            @isset($page->title)
-                                <h3>{{ $page['title'] }}</h3>
-                            @endisset
-                            <div class="line-clamp-5">
-                                {!! strip_tags(Illuminate\Mail\Markdown::parse($post['content'] ?? '')->toHtml()) !!}
-                            </div>
-                            <a class="inline-block font-semibold border-b-2 border-white" href="">Read</a>
-                        </li>
-                    @endforeach
-                </ol>
-            @endunless
+    <section class="max-w-6xl px-4 py-10 mx-auto">
+        <div class="px-6 py-4 text-white rounded-md shadow-xl bg-sky-500">
+            <div class="container space-y-5">
+                <h2>Recent Blog Posts</h2>
+                @unless($posts->isEmpty())
+                    <ol class="grid gap-6 md:grid-cols-3">
+                        @foreach ($posts as $post)
+                            <li class="space-y-2">
+                                @isset($page->title)
+                                    <h3>{{ $page['title'] }}</h3>
+                                @endisset
+                                <div class="line-clamp-5">
+                                    {!! strip_tags(Illuminate\Mail\Markdown::parse($post['content'] ?? '')->toHtml()) !!}
+                                </div>
+                                <a class="inline-block font-semibold border-b-2 border-white" href="">Read</a>
+                            </li>
+                        @endforeach
+                    </ol>
+                @endunless
+            </div>
         </div>
     </section>
 </x-web-layout>
