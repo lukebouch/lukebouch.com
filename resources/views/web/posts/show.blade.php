55bce4102df875b4d74bc892f8eb93a5ca1729b3
Luke Bouch <luke@lukebouch.com>
2022-06-13 19:08:01 -0400

    Add back to posts button

--- resources/views/web/posts/show.blade.php
+++ resources/views/web/posts/show.blade.php
@@ -12,5 +12,14 @@
                 {!! $post['html_content'] !!}
             </div>
         </div>
+        <div class="mt-8">
+            <a href="{{ route('posts.index') }}" class="flex items-center justify-center gap-3 text-gray-900">
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                </svg>
+                Posts
+            </a>
+        </div>
     </div>
 </x-web-layout>
