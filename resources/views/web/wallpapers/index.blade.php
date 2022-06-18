f39c807fc99b61de2a49bdd45bb80476a1d079bb
Luke Bouch <luke@lukebouch.com>
2022-06-18 13:01:26 -0400

    Add wallpaper (#21)
    * Install Media Library
    
    * Add Wallpaper model
    
    * Add additional field to Wallpaper Nova resource
    
    * Add wallpaper page
    
    * Setup S3 disk
    
    * Update WallpaperFactory.php

---
+++ resources/views/web/wallpapers/index.blade.php
@@ -0,0 +1,25 @@
+<x-web-layout>
+    <section class="flex items-center justify-center px-4 pb-24 text-center md:pb-40 py-28">
+        <h1 class="text-3xl md:text-5xl">Exceptional Wallpaper
+            <div class="mt-3 text-xl">for
+                <x-icons.phone /> Phone +
+                <x-icons.tablet /> Table +
+                <a href="/admin">
+                    <x-icons.computer />
+                </a> Computer
+            </div>
+        </h1>
+    </section>
+    <section class="container px-4">
+        <ul class="grid gap-10 mb-8 md:grid-cols-3">
+            @foreach ($wallpapers as $wallpaper)
+                <li class="grid gap-5">
+                    <img class="shadow-lg rounded-xl" src="{{ $wallpaper->media('wallpapers')->first()->getUrl() }}"
+                        alt="">
+                    <a class="mx-auto btn" href="{{ route('wallpapers.download', [$wallpaper]) }}">Download</a>
+                </li>
+            @endforeach
+        </ul>
+        {{ $wallpapers->links() }}
+    </section>
+</x-web-layout>
