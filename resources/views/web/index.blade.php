b1c90f75196170419409e4a3525d31971a3d871c
Luke Bouch <luke@lukebouch.com>
2022-10-16 16:00:45 -0400

    Add basic intro

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1,39 +1,10 @@
 <x-web-layout>
-    <section class="container flex items-center px-6">
-        <div class="flex-1">
-            <h1 class="h-card" rel="me" href="https://lukebouch.com/">Luke Bouch</h1>
-            <p>I'm a junior developer at Wilber Group where I aid in the development and maintance of custom business
-                applications.</p>
-        </div>
-        <div class="w-1/2 md:w-1/3 flex-inital">
-            <img class=" drop-shadow-md" src="/images/lukebouch-cutout.png" alt="Luke Bouch">
-        </div>
-    </section>
-    <section class="max-w-6xl px-4 mx-auto">
-        <div class="px-6 py-8 text-white rounded-md shadow-xl bg-sky-500">
-            <div class="container space-y-5">
-                <h2>Recent Blog Posts</h2>
-                @unless($posts->isEmpty())
-                    <ol class="grid gap-6 md:grid-cols-3">
-                        @foreach ($posts as $post)
-                            <li class="space-y-2">
-                                @isset($page->title)
-                                    <h3>{{ $page->title }}</h3>
-                                @endisset
-                                <div>
-                                    <a class="font-medium"
-                                        href="{{ route('posts.show', [$post->slug]) }}">{{ $post->published_at->setTimezone('America/New_York')->format('g:ia \o\n F j, Y') }}</a>
-                                </div>
-                                <div class="line-clamp-5">
-                                    {{ $post->text }}
-                                </div>
-                                <a class="inline-block font-semibold border-b-2 border-white"
-                                    href="{{ route('posts.show', [$post->slug]) }}">Read</a>
-                            </li>
-                        @endforeach
-                    </ol>
-                @endunless
-            </div>
-        </div>
+    <section class="p-5 md:px-14 mx-auto">
+        <img class="rounded-full w-16" src="{{ asset('images/profile-small.png') }}"/>
+        <h1 class="mt-5 text-4xl font-bold tracking-tight leading-snug text-zinc-900">Web Developer, Tech
+            Enthusiast<br/> and Follower of Christ.</h1>
+        <p class="text-zinc-600 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in odio ut eros venenatis hendrerit. Integer
+            sed bibendum neque. In rhoncus posuere quam, eget vehicula urna mollis sit amet. Nunc consequat eleifend
+            sagittis. Vestibulum elementum finibus ligula ut aliquet.</p>
     </section>
 </x-web-layout>
