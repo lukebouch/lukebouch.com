ef7fc26355e0deb3401211f3191a30e29a3b5880
Luke Bouch <luke@lukebouch.com>
2022-10-17 21:13:27 -0400

    Add description

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -3,10 +3,9 @@
         <img class="rounded-full w-16" src="{{ asset('images/profile-small.png') }}"/>
         <h1 class="mt-5 text-4xl font-bold tracking-tight leading-snug text-zinc-900">Web Developer, Tech
             Enthusiast<br/> and Follower of Christ.</h1>
-        <p class="text-zinc-600 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in odio
-            ut eros venenatis hendrerit. Integer
-            sed bibendum neque. In rhoncus posuere quam, eget vehicula urna mollis sit amet. Nunc consequat eleifend
-            sagittis. Vestibulum elementum finibus ligula ut aliquet.</p>
+        <p class="text-zinc-600 leading-relaxed">Homeschooled by my mom and equipped by my dad with a vast knowledge of engines and electricity, I have been
+            able to explore and enjoy numerous hobbies that keep me busy in between work and serving at church. I enjoy using well-designed software and have a passion for creating
+            great UI/UX.</p>
 
         <div class="mt-16 -mx-48 grid grid-cols-5 gap-14">
             <img class="my-auto rounded-md shadow-md rotate-6"
