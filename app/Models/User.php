eb9f3cbd2b8e95cd439c2ac5ee4a4da968fbd6f3
Luke Bouch <luke@lukebouch.com>
2022-10-21 23:42:46 -0400

    Add authorization for Filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -45,5 +46,15 @@ class User extends Authenticatable
     public function getTimeZoneAttribute()
     {
         return 'America/New_York';
+    }
+
+    public function canAccessFilament(): bool
+    {
+        // TODO: Test this
+        if (!app()->environment('production')) {
+            return true;
+        }
+
+        return $this->email == 'luke@lukebouch.com';
     }
 }
