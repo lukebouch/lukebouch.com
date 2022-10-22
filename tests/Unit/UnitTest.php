3eed15787a9ab7a6516d7a92c77e6a3ac26f780e
Luke Bouch <luke@lukebouch.com>
2022-10-22 10:45:09 -0400

    Add dummy unit test

---
+++ tests/Unit/UnitTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UnitTest extends TestCase
+{
+    public function testBasic()
+    {
+        $this->assertTrue(true);
+    }
+}
