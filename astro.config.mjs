641cda6265e272b6eb516e9f4ba1095a274c6694
Luke Bouch <luke@lukebouch.com>
2023-05-07 18:26:45 -0400

    Prefetch links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import prefetch from "@astrojs/prefetch";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://lukebouch.com',
-    integrations: [tailwind(), sitemap()]
+  site: 'https://lukebouch.com',
+  integrations: [tailwind(), sitemap(), prefetch()]
 });
