eec9fd030227de483255ad8a0df83b2e1446dc60
Luke Bouch <luke@lukebouch.com>
2023-05-07 17:52:39 -0400

    Add rss feed

---
+++ src/pages/feed.xml.js
@@ -0,0 +1,20 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import marked from '../utilities/marked';
+import sanitizeHtml from 'sanitize-html';
+
+export async function get(context) {
+    const posts = await getCollection('posts');
+
+    return rss({
+        title: 'Luke Bouch',
+        description: '',
+        site: context.site,
+        items: posts.map((post) => ({
+            title: post.data.title ?? post.slug + ' - micropost',
+            pubDate: post.data.publish_date,
+            content: sanitizeHtml(marked.parse(post.body)),
+            link: `/posts/${post.slug}/`,
+        })),
+    });
+}
