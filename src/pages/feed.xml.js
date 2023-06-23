c166f952b9b1246176d19460c6deb8d99c137c06
Luke Bouch <luke@lukebouch.com>
2023-06-23 12:12:41 -0400

    Remove post content from RSS feed

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,7 +1,6 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import marked from "../utilities/marked";
-import sanitizeHtml from "sanitize-html";
 
 export async function get(context) {
   const posts = await getCollection("posts");
@@ -13,9 +12,6 @@ export async function get(context) {
     items: posts.map((post) => ({
       title: post.data.title ?? post.slug + " - micropost",
       pubDate: post.data.publish_date,
-      content: sanitizeHtml(marked.parse(post.body), {
-        allowedTags: ["img"],
-      }),
       link: `/posts/${post.slug}/`,
     })),
   });
