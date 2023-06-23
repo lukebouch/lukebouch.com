b95e0ecf0936bd84ec81ba0a6b532e99952cfa52
Luke Bouch <luke@lukebouch.com>
2023-06-23 12:14:20 -0400

    Revert "Remove post content from RSS feed"
    This reverts commit c166f952b9b1246176d19460c6deb8d99c137c06.

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,6 +1,7 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import marked from "../utilities/marked";
+import sanitizeHtml from "sanitize-html";
 
 export async function get(context) {
   const posts = await getCollection("posts");
@@ -12,6 +13,9 @@ export async function get(context) {
     items: posts.map((post) => ({
       title: post.data.title ?? post.slug + " - micropost",
       pubDate: post.data.publish_date,
+      content: sanitizeHtml(marked.parse(post.body), {
+        allowedTags: ["img"],
+      }),
       link: `/posts/${post.slug}/`,
     })),
   });
