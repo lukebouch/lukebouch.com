a90663a345ebc5881b8da06d4541b111e1ffc386
Luke Bouch <luke@lukebouch.com>
2023-07-16 16:26:10 -0400

    Add type to collection

--- src/content/config.js
+++ src/content/config.js
@@ -1,6 +1,7 @@
 import { z, defineCollection } from "astro:content";
 
 const postsCollection = defineCollection({
+  type: "content",
   schema: z.object({
     title: z.string().nullable(),
     publish_date: z.optional(z.date()),
