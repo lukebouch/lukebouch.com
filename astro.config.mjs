86dc8a6703c159e011d5e1c799989bc383268535
Luke Bouch <luke@lukebouch.com>
2023-09-26 14:10:29 -0400

    Add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://lukebouch.com",
   integrations: [tailwind(), sitemap()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
